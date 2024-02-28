000ba178976fdbdb4851af81240ed2b15591aa86
Leftyluupy <99181178+Leftyluupy@users.noreply.github.com>
2024-02-28 19:03:17 +0900

    added handlebars language in app.js

--- app.js
+++ app.js
@@ -11,33 +11,20 @@ PORT = 9753; //this will change in the future apparently
 //database
 var db = require("./database/db-connector");
 
+//Citation: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%203%20-%20Integrating%20a%20Templating%20Engine%20(Handlebars)
+const { engine } = require("express-handlebars");
+var exphbs = require("express-handlebars");
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
 /*
     ROUTES
 */
-//Citation: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%200%20-%20Setting%20Up%20Node.js
+
+//Citation: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%203%20-%20Integrating%20a%20Templating%20Engine%20(Handlebars)
 
 app.get("/", function (req, res) {
-  // Citation: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%201%20-%20Connecting%20to%20a%20MySQL%20Database
-  query1 = "DROP TABLE IF EXISTS diagnostic;";
-  query2 =
-    "CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);";
-  query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working.");';
-  query4 = "SELECT * FROM diagnostic;";
-
-  //drop table
-  db.pool.query(query1, function (err, results, fields) {
-    //create table
-    db.pool.query(query2, function (err, results, fields) {
-      //insert into table
-      db.pool.query(query3, function (err, results, fields) {
-        //select * from
-        db.pool.query(query4, function (err, results, fields) {
-          //send results to browser
-          res.send(JSON.stringify(results));
-        });
-      });
-    });
-  });
+  res.render("index");
 });
 
 /*
