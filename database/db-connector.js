e10db60e15261ebd66ecfee3dc2dd0c992a478fc
Leftyluupy <99181178+Leftyluupy@users.noreply.github.com>
2024-03-01 12:07:38 +0900

    password changed

--- database/db-connector.js
+++ database/db-connector.js
@@ -8,7 +8,7 @@ var pool = mysql.createPool({
   connectionLimit: 10,
   host: "classmysql.engr.oregonstate.edu",
   user: "cs340_pascuccl",
-  password: "wYIfxjXmIUsS",
+  password: "clL4mCztkvPhuhkT",
   database: "cs340_pascuccl",
 });
 
