a142e2138e668503f81463cdf5746b592b0044eb
Leftyluupy <99181178+Leftyluupy@users.noreply.github.com>
2024-03-02 14:19:04 +0900

    made update player event listener

--- public/js/update_player.js
+++ public/js/update_player.js
@@ -1,1 +1,25 @@
-let updatePersonForm = document.getElementById('update-player-form-ajax')
+let updatePlayerForm = document.getElementById("update-player-form-ajax");
+
+updatePlayerForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let inputFullName = document.getElementById("mySelect");
+
+  let fullNameValue = inputFullName.value;
+
+  let data = {
+    fullname: fullNameValue,
+  };
+
+  var xhttp = newXMLHttpRequest();
+  xhttp.open("PUT", "/update-player-ajax", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      updateRow(xhttp.response, fullNameValue);
+    } else if (xhttp.readyState == 4 && xhttp.status != 200) {
+      console.log("There was an error with the input!");
+    }
+  };
+  xhttp.sent(JSON.stringify(data));
+});
